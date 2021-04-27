30d51468003b79f5239872f66369b189c4c1937b
Krzysztof Bokiej <krzysztof.bokiej@roche.com>
2021-04-27 23:41:20 +0200

    consent

--- js/init_main.js
+++ js/init_main.js
@@ -40,3 +40,19 @@ window.addEventListener('load', () => {
     typeof AOS !== 'undefined' ? AOS.init() : null;
     spinnerContainer.remove();
 });
+
+ez_consent.init(
+  {
+    is_always_visible: false,       // Always shows banner on load, default: false
+    texts: {
+      main: "Używamy plików cookies, aby ułatwić Ci korzystanie z naszego serwisu oraz do celów statystycznych. " +
+          "Korzystając z naszej strony wyrażasz zgodę na wykorzystywanie przez nas plików cookies. " +
+          "Jeśli nie blokujesz tych plików, to zgadzasz się na ich użycie oraz zapisanie w pamięci urządzenia. " +
+          "Pamiętaj, że możesz samodzielnie zarządzać cookies, zmieniając ustawienia przeglądarki.",       // The text that's shown on the banner, default: "This website uses cookies & similar."
+      buttons:
+      {
+        ok: "ok",                   // OK button to hide the text, default: "ok"
+        more: ""                // More button that shows the privacy policy, default "more"
+      }
+    }
+  });
